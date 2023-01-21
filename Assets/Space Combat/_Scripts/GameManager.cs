0fc0525ec7eee4fe1f7b697e7b3273fe401b35a1
AlexIancuSalford <73466319+AlexIancuSalford@users.noreply.github.com>
2023-01-21 15:59:58 +0000

    First commit

---
+++ Assets/Space Combat/_Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace SpaceCombat
+{
+    public class GameManager : MonoBehaviour
+    {
+        private ManualXRControl ManualXRControl { get; set; } = null;
+
+        private PlayerInput PlayerInput { get; set; } = null;
+        private InputAction Move { get; set; } = null;
+
+        private void Awake()
+        {
+            PlayerInput = GetComponent<PlayerInput>();
+            Move = PlayerInput.actions["Move"];
+            ManualXRControl = GetComponent<ManualXRControl>();
+        }
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+            Move.performed += (context) => { Debug.Log(Move.ReadValue<float>()); };
+            InputSystem.onDeviceChange += InputUserOnChange;
+        }
+
+        private void InputUserOnChange(InputDevice device, InputDeviceChange change)
+        {
+            if (change == InputDeviceChange.Added)
+            {
+                if(device.name.Contains("Vive") || 
+                   device.name.Contains("Oculus"))
+                {
+                    StartCoroutine(ManualXRControl.StartXRCoroutine());
+                }
+            }
+
+            if (change is InputDeviceChange.Disabled or InputDeviceChange.Removed)
+            {
+                if(device.name.Contains("Vive") || 
+                   device.name.Contains("Oculus"))
+                {
+                    ManualXRControl.StopXR();
+                }
+            }
+        }
+    }
+}
